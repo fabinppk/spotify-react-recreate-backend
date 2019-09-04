9a2f658ff1835f51f1b56c3003481d9c8bd188b9
Fabiano <fabinppk@hotmail.com>
2019-09-04 14:30:14 -0300

    feat: creating store / redux config

---
+++ src/store/ducks/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+// reducers
+import playlists from './playlists';
+import playlistDetails from './playlistDetails';
+import error from './error';
+import player from './player';
+
+export default combineReducers({
+    playlists,
+    playlistDetails,
+    error,
+    player
+});
